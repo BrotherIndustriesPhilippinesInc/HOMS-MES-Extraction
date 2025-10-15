6ab3358acca4cf829da1a26d851cf8b273ed8398
appli-arengama <marcracille.arenga@brother-biph.com.ph>
2025-10-15 11:57:57 +0800

    Init Commit

---
+++ HOMS MES Extractor Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HOMS_MES_Extractor_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ HOMS MES Extractor/Main.cs
@@ -0,0 +1,10 @@
+namespace HOMS_MES_Extractor
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
